4a6f76a1cdd581bb6b9370cacef09a9e83ce4caf
Md. Wadud Mahmud Joy <wadudjoy24@gmail.com>
2025-12-26 01:22:40 +0600

    Add GitHub Actions workflow and fix Android build config

--- weather_app/android/app/build.gradle.kts
+++ weather_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.sira.forecasting"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- weather_app/android/app/src/main/kotlin/com/sira/forecasting/MainActivity.kt
+++ weather_app/android/app/src/main/kotlin/com/sira/forecasting/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weather_app
+package com.sira.forecasting
 
 import io.flutter.embedding.android.FlutterActivity
 
