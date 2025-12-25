77664ba4c1c02b89d23c2ca8b235834b4cbc2b10
Md. Wadud Mahmud Joy <wadudjoy24@gmail.com>
2025-12-26 01:52:52 +0600

    Use consistent debug keystore for reproducible builds

--- weather_app/android/app/build.gradle.kts
+++ weather_app/android/app/build.gradle.kts
@@ -32,6 +32,15 @@ android {
         multiDexEnabled = true 
     }
 
+    signingConfigs {
+        create("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
